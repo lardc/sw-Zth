d9111395c74477f40098cd3f1feb4b5bbe9c8e8b
SCMEVan <vansup111@gmail.com>
2020-06-08 12:57:05 +0300

    Настройка графика на тестовой странице

--- Zth/TestWindow.xaml.cs
+++ Zth/TestWindow.xaml.cs
@@ -4,6 +4,7 @@ using LiveCharts.Defaults;
 using LiveCharts.Wpf;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,8 +26,13 @@ namespace Zth
         public Func<double, string> Formatter { get; set; }
         public double Base { get; set; }
 
+        public string[] Labels { get; set; } = new string[] { "55", "60", "70" , "80" ,"90" ,"c"  };
+
+        public Func<double, string> FormatterDegreesCelsius { get; set; }
+
         public TestWindow()
         {
+            //Labels.Reverse();
             InitializeComponent();
 
             Base = 10;
@@ -35,26 +41,43 @@ namespace Zth
                 .X(point => Math.Log(point.X, Base)) //a 10 base log scale in the X axis
                 .Y(point => point.Y);
 
-            SeriesCollection = new SeriesCollection(mapper)
+            var MariaSeries = new LineSeries
             {
-                new LineSeries
+                Values = new ChartValues<ObservablePoint>
                 {
-                    Values = new ChartValues<ObservablePoint>
-                    {
-                        new ObservablePoint(0.00001, 2),
-                        new ObservablePoint(0.0005, 5),
-                        new ObservablePoint(0.001, 1),
-                        new ObservablePoint(0.01, 3),
-                        new ObservablePoint(0.1, 8),
-                        new ObservablePoint(1, 5),
+                    new ObservablePoint(0.00001, 2),
+                    new ObservablePoint(0.00003, 4),
+                    new ObservablePoint(0.00008, 1),
+                    /*               new ObservablePoint(0.0005, 5),
+                                   new ObservablePoint(0.001, 1),
+                                   new ObservablePoint(0.01, 3),
+                                   new ObservablePoint(0.1, 8),*/
+                    /*new ObservablePoint(1, 5),*/
 
-                    }
-                }
+                },
             };
 
-            Formatter = value => Math.Pow(Base, value).ToString();
+            SeriesCollection = new SeriesCollection(mapper)
+            {
+                MariaSeries
+
+
+            };
+
+            Formatter = value => value > 0 ? "сек" : Math.Pow(Base, value).ToString();
+            FormatterDegreesCelsius = value =>
+            {
+                if (value == 95)
+                    return Properties.Resource.UnitMeasurementDegreeCentigrade;
+                else if (new double[] { 60, 70, 80, 90 }.Contains(value))
+                    return value.ToString();
+                return string.Empty;
+            };
+
 
             DataContext = this;
         }
+
+      
     }
 }
