372506e7608afee69e5c1fc6a9aa4a30b94c9387
SCMEVan <vansup111@gmail.com>
2020-06-08 19:20:50 +0300

    Настройка главной страницы для демонстрации

--- Zth/MainWindow.xaml.cs
+++ Zth/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Navigation;
+﻿using System;
+using System.Windows.Navigation;
 using Zth.VM;
 
 namespace Zth
@@ -11,16 +12,26 @@ namespace Zth
         private readonly NavigationService _navigationService;
         public TopPanelVm TopPanelVm { get; set; } = new TopPanelVm()
         {
-            DataIsVisibly = true
+            DataIsVisibly = true,
+            WorkingMode = WorkingMode.RthSequence
         };
-        public CommonVM FrameVm { get; set; }
+        public CommonVM FrameVm { get; set; } = new CommonVM();
+        public BottomPanelVM BottomPanelVM { get; set; } = new BottomPanelVM();
         public MainWindow()
         {
             InitializeComponent();
+
+
             _navigationService = NavigationService.GetNavigationService(this);
             _navigationService = MainFrame.NavigationService;
-            //_navigationService.Source = new System.Uri("Pages/SelectPage.xaml", System.UriKind.Relative);
-            _navigationService.Navigate(new Pages.ZthLongImpulse()
+            _navigationService.Navigate(new Pages.SelectPage()
+            {
+                DataContext = TopPanelVm
+            });
+
+            BottomPanelVM.LeftButtonContent = Properties.Resource.Back;
+            BottomPanelVM.RightButtonContent = Properties.Resource.Next;
+            /*_navigationService.Navigate(new Pages.ZthLongImpulse()
             {
                 DataContext = FrameVm = new CommonVM()
                 {
@@ -31,12 +42,79 @@ namespace Zth
                     AnodeCoolerTemperatureIsVisibly = true,
                     CathodeCoolerTemperatureIsVisibly = true
                 }
-            });
+            });*/
         }
 
         private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
         {
+            FrameVm = new CommonVM();
 
+            switch (TopPanelVm.WorkingMode)
+            {
+                case WorkingMode.ZthLongImpulse:
+
+                    FrameVm.HeatingCurrentIsVisibly = true;
+                    FrameVm.HeatingPowerIsVisibly = true;
+                    FrameVm.AnodeBodyTemperatureIsVisibly = true;
+                    FrameVm.CathodeBodyTemperatureIsVisibly = true;
+                    FrameVm.AnodeCoolerTemperatureIsVisibly = true;
+                    FrameVm.CathodeCoolerTemperatureIsVisibly = true;
+                    _navigationService.Navigate(new Pages.ZthLongImpulse()
+                    {
+                        DataContext = FrameVm
+                    });
+                    BottomPanelVM.RightButtonContent = "Градуировка";
+                    break;
+                case WorkingMode.ZthSequence:
+                    FrameVm.HeatingCurrentIsVisibly = true;
+                    FrameVm.HeatingPowerIsVisibly = true;
+                    FrameVm.TemperatureSensitiveParameterIsVisibly = true;
+                    FrameVm.AnodeBodyTemperatureIsVisibly = true;
+                    FrameVm.CathodeBodyTemperatureIsVisibly = true;
+                    FrameVm.AnodeCoolerTemperatureIsVisibly = true;
+                    FrameVm.CathodeCoolerTemperatureIsVisibly = true;
+                    _navigationService.Navigate(new Pages.ZthPulseSequence()
+                    {
+                        DataContext = FrameVm
+                    });
+                    BottomPanelVM.RightButtonContent = "Градуировка";
+                    break;
+                case WorkingMode.RthSequence:
+                    FrameVm.HeatingCurrentIsVisibly = true;
+                    FrameVm.HeatingPowerIsVisibly = true;
+                    FrameVm.TemperatureSensitiveParameterIsVisibly = true;
+                    FrameVm.AnodeBodyTemperatureIsVisibly = true;
+                    FrameVm.CathodeBodyTemperatureIsVisibly = true;
+                    FrameVm.AnodeCoolerTemperatureIsVisibly = true;
+                    FrameVm.CathodeCoolerTemperatureIsVisibly = true;
+                    _navigationService.Navigate(new Pages.RthPulseSequence()
+                    {
+                        DataContext = FrameVm
+                    });
+                    BottomPanelVM.RightButtonContent = "Градуировка";
+                    break;
+                case WorkingMode.GraduationOnly:
+                    FrameVm.TemperatureSensitiveParameterIsVisibly = true;
+                    FrameVm.AnodeBodyTemperatureIsVisibly = true;
+                    FrameVm.CathodeBodyTemperatureIsVisibly = true;
+                    FrameVm.AnodeCoolerTemperatureIsVisibly = true;
+                    FrameVm.CathodeCoolerTemperatureIsVisibly = true;
+                    _navigationService.Navigate(new Pages.GraduationOnly()
+                    {
+                        DataContext = FrameVm
+                    });
+                    BottomPanelVM.MiddleButtonContent = "Загрузка из файла";
+                    BottomPanelVM.RightButtonContent = "Расчёт градуировки";
+                    break;
+
+
+            }
+        }
+
+        private void Button_Click_1(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if(_navigationService.CanGoBack)
+                _navigationService.GoBack();
         }
     }
 }
