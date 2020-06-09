53ee8fb26b1b62b8e3cc531fdc272c01eab15182
SCMEVan <vansup111@gmail.com>
2020-06-09 20:16:24 +0300

    Компонент графика, настраиваемый

---
+++ Zth/Components/Chart.xaml.cs
@@ -0,0 +1,36 @@
+﻿using LiveCharts;
+using LiveCharts.Configurations;
+using LiveCharts.Defaults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zth.Components
+{
+    /// <summary>
+    /// Логика взаимодействия для Chart.xaml
+    /// </summary>
+    public partial class Chart : UserControl
+    {
+     
+
+
+
+        public Chart()
+        {
+           
+            InitializeComponent();
+
+        }
+    }
+}
